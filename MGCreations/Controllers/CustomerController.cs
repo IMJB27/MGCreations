80c8d857170c4b32decfecfaa26977444cee3f67
Jay Bharakhada <bharakhdajay@gmail.com>
2022-03-16 19:20:25 +0000

    complete customer add, update, delete and view

--- MGCreations/Controllers/CustomerController.cs
+++ MGCreations/Controllers/CustomerController.cs
@@ -69,5 +69,86 @@ namespace MGCreations.Controllers
         {
             return View(db.customer_details.ToList());
         }
+
+        [HttpGet]
+        public ActionResult Customer_Details(int? c_id)
+        {
+            if (c_id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            customer_details customer = db.customer_details.Find(c_id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(customer);
+        }
+
+        [HttpGet]
+        public ActionResult Update_Customer_Details(int? c_id)
+        {
+            if (c_id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            customer_details customer = db.customer_details.Find(c_id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(customer);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Update_Customer_Details(customer_details customer)
+        {
+            customer_details c = new customer_details();
+            c.Customer_Username = customer.Customer_Username;
+            c.Customer_Password = customer.Customer_Password;
+            c.Customer_Name = customer.Customer_Name;
+            c.Customer_Surname = customer.Customer_Surname;
+            c.Customer_DOB = customer.Customer_DOB;
+            c.Customer_Email = customer.Customer_Email;
+            c.Customer_ContactNo = customer.Customer_ContactNo;
+            c.Customer_AddressLine1 = customer.Customer_AddressLine1;
+            c.Customer_AddressLine2 = customer.Customer_AddressLine2;
+            c.Customer_City = customer.Customer_City;
+            c.Customer_County = customer.Customer_County;
+            c.Customer_Country = customer.Customer_Country;
+            c.Customer_PostCode = customer.Customer_PostCode;
+            db.Entry(customer).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Customer_List");
+        }
+
+        [HttpGet]
+        public ActionResult Delete_Customer(int? c_id)
+        {
+            if (c_id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            customer_details customer = db.customer_details.Find(c_id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        [HttpPost, ActionName("Delete_Customer")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int c_id)
+        {
+            customer_details customer = db.customer_details.Find(c_id);
+            db.customer_details.Remove(customer);
+            db.SaveChanges();
+            return RedirectToAction("Customers_List");
+        }
     }
 }
